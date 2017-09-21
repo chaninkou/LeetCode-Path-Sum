5112c976ed1135833b64489bff3d2d15cefa0dd5
chaninkou <michaelkoulove@yahoo.com>
2017-09-20 22:31:44 -0700

    This is an exercise for java

---
+++ Example10.java
@@ -0,0 +1,19 @@
+/*   
+Author: Chan In Kou
+Purpose: The Display will show a table of powers of 2. 
+*/
+package example10;
+
+public class Example10 {
+	public static void main(String[] args) {
+		System.out.println("Power      Result");
+		System.out.println("2^0        1");
+		System.out.println("2^1        2");
+		System.out.println("2^2        4");
+		System.out.println("2^3        8");
+		System.out.println("2^4        16");
+		System.out.println("2^5        32");
+		System.out.println("2^6        64");
+		System.out.println("2^7        128");
+	}
+}
